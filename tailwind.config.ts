18f87de5e572ce65cfae1d0e9b7ae7b8b791ba32
Tarp <travistarpley0@gmail.com>
2025-01-15 17:13:48 -0800

    feat: configuring theme colors, adding grays

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ export default {
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+
         card: {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
@@ -26,6 +27,17 @@ export default {
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
+        },
+        gray: {
+          100: "hsl(var(--gray-100), <alpha-value>)",
+          200: "hsl(var(--gray-200), <alpha-value>)",
+          300: "hsl(var(--gray-300), <alpha-value>)",
+          400: "hsl(var(--gray-400), <alpha-value>)",
+          500: "hsl(var(--gray-500), <alpha-value>)",
+          600: "hsl(var(--gray-600), <alpha-value>)",
+          700: "hsl(var(--gray-700), <alpha-value>)",
+          800: "hsl(var(--gray-800), <alpha-value>)",
+          900: "hsl(var(--gray-900), <alpha-value>)",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
