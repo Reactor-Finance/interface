271fd604581edd8965a44263e8bc085d3bcef404
Tarp <travistarpley0@gmail.com>
2025-01-24 11:55:51 -0800

    feat: added jetbrains-mono font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Providers } from "./providers";
+import { JetBrains_Mono } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const jetbrainsMono = JetBrains_Mono({});
 
 export const metadata: Metadata = {
   title: "Reactor Finance",
@@ -26,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={` ${jetbrainsMono.className} antialiased`}>
         <Providers>
           <main className="flex-1">{children}</main>
         </Providers>
