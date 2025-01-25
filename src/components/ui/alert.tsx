38c660432cd85c3170fd102f9a6c6b937b370b30
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2025-01-24 21:10:09 -0800

    feat: added alert component

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -2,19 +2,21 @@ import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+import { CircleAlert } from "lucide-react";
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+  `relative w-full rounded-lg p-4 [&>svg~*]:pl-7 
+   [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground`,
   {
     variants: {
-      variant: {
-        default: "bg-background text-foreground",
-        destructive:
-          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+      colors: {
+        yellow: "bg-yellow/15 text-yellow",
+        error: "bg-error-400/15 text-error-400",
+        success: "bg-success-400/15 text-success-400",
       },
     },
     defaultVariants: {
-      variant: "default",
+      colors: "yellow",
     },
   }
 );
@@ -22,13 +24,20 @@ const alertVariants = cva(
 const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
->(({ className, variant, ...props }, ref) => (
+>(({ className, children, colors, ...props }, ref) => (
   <div
     ref={ref}
     role="alert"
-    className={cn(alertVariants({ variant }), className)}
+    className={cn(alertVariants({ colors }), className)}
     {...props}
-  />
+  >
+    <div className="flex gap-x-2">
+      <div className="pt-[3px]">
+        <CircleAlert className={` w-5 h-5`} />
+      </div>
+      <div>{children}</div>
+    </div>
+  </div>
 ));
 Alert.displayName = "Alert";
 
