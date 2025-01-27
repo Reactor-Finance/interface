98df92f7546829bb8213dbe1ab094dd52610ab1f
Tarp <travistarpley0@gmail.com>
2025-01-27 11:11:23 -0800

    feat: adjust image size

--- src/components/shared/currenciesOverlapIcons.tsx
+++ src/components/shared/currenciesOverlapIcons.tsx
@@ -13,20 +13,19 @@ interface Props {
   };
 }
 export default function CurrenciesOverlapIcons({ tokenOne, tokenTwo }: Props) {
-  console.log(getLogoAsset(tokenOne.address));
   return (
     <div className="flex">
       <ImageWithFallback
         className="h-8 w-8"
-        width={32}
-        height={32}
+        width={35}
+        height={35}
         src={getLogoAsset(tokenOne.address)}
         alt={tokenOne.alt}
       />
       <ImageWithFallback
         className="h-8 w-8 -ml-2 z-10"
-        width={32}
-        height={32}
+        width={35}
+        height={35}
         src={getLogoAsset(tokenTwo.address)}
         alt={tokenTwo.alt}
       />
