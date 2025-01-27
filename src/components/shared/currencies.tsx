b7cc6f5ae6cf1034738561ea7639f1d44c589ee9
Tarp <travistarpley0@gmail.com>
2025-01-27 10:38:53 -0800

    feat: added styles, fixed props

--- src/components/shared/currencies.tsx
+++ src/components/shared/currencies.tsx
@@ -5,21 +5,28 @@ import { TAddress } from "@/lib/types";
 interface Props {
   tokenOne: {
     address: TAddress;
-    alt: TAddress;
+    alt: string;
   };
   tokenTwo: {
     address: TAddress;
-    alt: TAddress;
+    alt: string;
   };
 }
 export default function Currencies({ tokenOne, tokenTwo }: Props) {
+  console.log(getLogoAsset(tokenOne.address));
   return (
-    <div>
+    <div className="flex">
       <ImageWithFallback
+        className="h-6 w-6"
+        width={24}
+        height={24}
         src={getLogoAsset(tokenOne.address)}
         alt={tokenOne.alt}
       />
       <ImageWithFallback
+        className="h-6 w-6 -ml-2 z-10"
+        width={24}
+        height={24}
         src={getLogoAsset(tokenTwo.address)}
         alt={tokenTwo.alt}
       />
