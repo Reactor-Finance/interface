e069d91a59f7e9adc9331bd70fd4a38f96e7f9e8
Khanh <khanhtrandev16302@gmail.com>
2025-02-05 19:07:19 +0100

    fix: fixed wrong commit.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 import SearchTokensDailog from "./dex/searchTokensDialog";
-import Toggle from "@/components/ui/toggle";
 
 export default function Home() {
   return (
     <div className="min-h-screen font-geistMono ">
       <SearchTokensDailog />
-      <div className="flex items-center justify-between">
-        <Toggle className="" label="Label" isDisabled={false} />
-        <Toggle className="" label="Disabled_Label" isDisabled={true} />
-        <Toggle className="" label="Label" isDisabled={false} />
-        <Toggle className="" isDisabled={true} />
-      </div>
     </div>
   );
 }
