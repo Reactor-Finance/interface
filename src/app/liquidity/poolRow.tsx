9857220f2489a39b5949298b641360314ba45a33
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2025-02-08 09:59:48 -0800

    feat: added reusable component pool header

--- src/app/liquidity/poolRow.tsx
+++ src/app/liquidity/poolRow.tsx
@@ -1,5 +1,5 @@
 import CurrenciesOverlapIcons from "@/components/shared/currenciesOverlapIcons";
-import { Badge } from "@/components/ui/badge";
+import PoolHeader from "@/components/shared/poolHeader";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
@@ -7,30 +7,7 @@ export default function PoolRow() {
   return (
     <tr className="grid grid-cols-11 items-center text-sm text-right gap-x-4 py-4 bg-neutral-1050 px-4 rounded-md">
       <th className="col-span-4 text-left">
-        <div className="flex gap-x-4 items-center">
-          <span>1</span>
-          <div className="flex gap-x-2 items-center">
-            <CurrenciesOverlapIcons
-              tokenOne={{
-                alt: "",
-                address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-              }}
-              tokenTwo={{
-                alt: "",
-                address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-              }}
-            />
-            <div>
-              <h4>vAMM-ETH/USDC</h4>
-              <div className="space-x-1">
-                <Badge>Concetrated</Badge>
-                <Badge border="one" colors="neutral">
-                  0.3%
-                </Badge>
-              </div>
-            </div>
-          </div>
-        </div>
+        <PoolHeader />
       </th>
       <th className="">$5,505,444</th>
       <th className="text-blue-light">11%</th>
