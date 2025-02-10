a3fcd062da7e54e1cc70e0edd8a30e65a5ce9b78
Tarp <travistarpley0@gmail.com>
2025-02-09 18:21:19 -0800

    feat: added add-liq page

---
+++ src/app/liquidity/add-liquidity/page.tsx
@@ -0,0 +1,6 @@
+import PageMarginContainer from "@/components/ui/pageMarginContainer";
+import React from "react";
+
+export default function Page() {
+  return <PageMarginContainer>Page</PageMarginContainer>;
+}
