6b57f21e82c9a96aff2d32b85f0ea820a815a4a9
Tarp <travistarpley0@gmail.com>
2025-02-09 17:40:08 -0800

    fix: fixed hydration error from child div inside table

--- src/app/liquidity/poolsTable.tsx
+++ src/app/liquidity/poolsTable.tsx
@@ -3,41 +3,42 @@ import { ChevronRight, ChevronLeft } from "lucide-react";
 import PoolRow from "./poolRow";
 export default function PoolsTable() {
   return (
-    <table className="w-full">
-      <thead className="text-neutral-400 text-sm text-right w-full">
-        <tr className=" grid grid-cols-11 gap-x-4 px-4">
-          <th className="col-span-4 text-left flex gap-x-4">
-            <span>#</span> <span>Pool Name</span>
-          </th>
-          <th className="">TVL</th>
-          <th>APR</th>
-          <th>Volume</th>
-          <th>Fees</th>
-          <th className="text-left col-span-3 ">Liquidity Manager</th>
-        </tr>
-      </thead>
-      <tbody className="gap-y-2 pt-2 flex flex-col">
-        <PoolRow />
-        <PoolRow />
-        <PoolRow />
-        <PoolRow />
-      </tbody>
-      <tfoot className="pt-2 block">
-        <div className="flex justify-between">
-          <p className="text-[13px]">
-            Page 1 of 34{" "}
-            <span className="text-neutral-300">(1 - 250 results)</span>
-          </p>
-          <div className="flex">
-            <button>
-              <ChevronLeft className="text-white" />
-            </button>
-            <button>
-              <ChevronRight className="text-white" />
-            </button>
-          </div>
+    <>
+      <table className="w-full">
+        <thead className="text-neutral-400 text-sm text-right w-full">
+          <tr className=" grid grid-cols-11 gap-x-4 px-4">
+            <th className="col-span-4 text-left flex gap-x-4">
+              <span>#</span> <span>Pool Name</span>
+            </th>
+            <th className="">TVL</th>
+            <th>APR</th>
+            <th>Volume</th>
+            <th>Fees</th>
+            <th className="text-left col-span-3 ">Liquidity Manager</th>
+          </tr>
+        </thead>
+        <tbody className="gap-y-2 pt-2 flex flex-col">
+          <PoolRow />
+          <PoolRow />
+          <PoolRow />
+          <PoolRow />
+        </tbody>
+      </table>
+
+      <div className="pt-2 block">
+        <p className="text-[13px]">
+          Page 1 of 34{" "}
+          <span className="text-neutral-300">(1 - 250 results)</span>
+        </p>
+        <div className="flex">
+          <button>
+            <ChevronLeft className="text-white" />
+          </button>
+          <button>
+            <ChevronRight className="text-white" />
+          </button>
         </div>
-      </tfoot>
-    </table>
+      </div>
+    </>
   );
 }
