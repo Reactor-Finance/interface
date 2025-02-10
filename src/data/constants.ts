b1f761e759390a623d059fc3e34c65398cb238e5
Tarp <travistarpley0@gmail.com>
2025-02-10 11:05:20 -0800

    fix: don't import env server on client side

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,6 +1,3 @@
-import { env } from "@/app/env";
-
 export const ASSET_REPO =
   "https://raw.githubusercontent.com/SIR-trading/assets/master";
 export const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-export const GRAPHQL_URL = env.SUBGRAPH_URL || "http://localhost:4000/graphql";
