e1ab034fba3e413d0c75ed931b8f8093ceaca81b
Tarp <travistarpley0@gmail.com>
2025-02-10 17:22:44 -0800

    feat: add select token one and two logic

--- src/app/liquidity/deposit/page.tsx
+++ src/app/liquidity/deposit/page.tsx
@@ -2,13 +2,38 @@
 import { Card } from "@/components/ui/card";
 import Headers from "@/components/ui/headers";
 import PageMarginContainer from "@/components/ui/pageMarginContainer";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Alert } from "@/components/ui/alert";
 import { ChevronDown } from "lucide-react";
-import SearchTokensDailog from "@/app/dex/searchTokensDialog";
-
+import SearchTokensDailog from "@/components/shared/searchTokensDialog";
+import ImageWithFallback from "@/components/shared/imageWithFallback";
+import { getLogoAsset } from "@/utils";
+import { TAddress } from "@/lib/types";
+type TToken = { address: string; symbol: string };
 export default function Page() {
-  const [open, setOpen] = useState(false);
+  const [openOne, setOpenTokenOne] = useState(false);
+  const [openTwo, setOpenTokenTwo] = useState(false);
+  const [tokenOne, setTokenOne] = useState<TToken | undefined>();
+  const [tokenTwo, setTokenTwo] = useState<TToken | undefined>();
+  console.log(tokenTwo, tokenOne, "TOKENS");
+  const setToken = ({ address, symbol }: TToken) => {
+    if (openOne) {
+      setTokenOne({ address, symbol });
+      setOpenTokenOne(false);
+    }
+    if (openTwo) {
+      setTokenTwo({ address, symbol });
+      setOpenTokenTwo(false);
+    }
+  };
+  const setOpen = useCallback(() => {
+    if (openOne) {
+      setOpenTokenOne(false);
+    }
+    if (openTwo) {
+      setOpenTokenTwo(false);
+    }
+  }, [openOne, openTwo]);
   return (
     <PageMarginContainer>
       <Headers.GradiantHeaderOne colorOne="#A0055D" colorTwo="#836EF9">
@@ -16,12 +41,22 @@ export default function Page() {
       </Headers.GradiantHeaderOne>
       <div className="pt-6"></div>
       <div className=" gap-x-4 grid grid-cols-2">
-        <SearchTokensDailog open={open} setOpen={setOpen} />
+        <SearchTokensDailog
+          setToken={setToken}
+          open={openOne || openTwo}
+          setOpen={setOpen}
+        />
         <Card bg="1000">
-          <SearchTokensTrigger onClick={() => setOpen(true)} />
+          <SearchTokensTrigger
+            token={tokenOne}
+            onClick={() => setOpenTokenOne(true)}
+          />
         </Card>
         <Card bg="1000">
-          <SearchTokensTrigger onClick={() => setOpen(true)} />
+          <SearchTokensTrigger
+            token={tokenTwo}
+            onClick={() => setOpenTokenTwo(true)}
+          />
         </Card>
       </div>
       <div className="pt-6">
@@ -34,7 +69,14 @@ export default function Page() {
   );
 }
 
-function SearchTokensTrigger({ onClick }: { onClick: () => void }) {
+function SearchTokensTrigger({
+  onClick,
+  token,
+}: {
+  onClick: () => void;
+  token?: TToken;
+}) {
+  console.log(token);
   return (
     <button
       onClick={() => {
@@ -42,7 +84,20 @@ function SearchTokensTrigger({ onClick }: { onClick: () => void }) {
       }}
       className="flex justify-between items-center py-2 bg-neutral-950 w-full rounded-md px-4"
     >
-      <span className="text-neutral-200">Select first token</span>
+      {token ? (
+        <div className="flex gap-x-2 items-center">
+          <span>{token.symbol}</span>
+          <ImageWithFallback
+            width={24}
+            height={24}
+            className="rounded-full h-5 w-5"
+            src={getLogoAsset(token.address as TAddress)}
+            alt={token.symbol}
+          />
+        </div>
+      ) : (
+        <span className="text-neutral-200">Select first token</span>
+      )}
       <div>
         <ChevronDown className="w-5 h-5" />
       </div>
