bc53bcfc8bd4d84418f3ceb7ecc2d3af6c3d9aa1
Tarp <travistarpley0@gmail.com>
2025-02-11 12:11:37 -0800

    fix: made mock token TToken type

--- src/data/mock/tokens.ts
+++ src/data/mock/tokens.ts
@@ -1,4 +1,6 @@
-export const tokens = [
+import { TAddress } from "@/lib/types";
+
+export const tokens: { symbol: string; address: TAddress }[] = [
   { symbol: "ETH", address: "0x0000000000000000000000000000000000000000" },
   { symbol: "USDT", address: "0xdAC17F958D2ee523a2206206994597C13D831ec7" },
   { symbol: "USDC", address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606EB48" },
