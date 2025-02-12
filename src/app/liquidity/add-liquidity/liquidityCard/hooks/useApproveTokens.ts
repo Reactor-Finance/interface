f0e0c16aff92b746e1f72fa38c75b6e6f05ea4ce
Tarp <travistarpley0@gmail.com>
2025-02-12 14:18:26 -0800

    fix: fixed approve simulation, added edge case abi

--- src/app/liquidity/add-liquidity/liquidityCard/hooks/useApproveTokens.ts
+++ src/app/liquidity/add-liquidity/liquidityCard/hooks/useApproveTokens.ts
@@ -1,5 +1,25 @@
 import { useSimulateContract } from "wagmi";
 import { useLiquidityCardFormProvider } from "../liquidityCardFormProvider";
+import { Contracts } from "@/lib/contracts";
+// altered abi for USDT edge case
+const abi = [
+  {
+    type: "function",
+    name: "approve",
+    stateMutability: "nonpayable",
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    outputs: [],
+  },
+];
 
 export default function useApproveTokens({
   approveTokenOne,
@@ -11,17 +31,18 @@ export default function useApproveTokens({
   const { tokenOne, tokenTwo } = useLiquidityCardFormProvider();
   const { data: approveTokenOneRequest } = useSimulateContract({
     address: tokenOne,
+    abi,
     functionName: "approve",
-    args: [tokenTwo, 0n],
-    query: { enabled: approveTokenOne },
+    args: [Contracts.Router.address, 10000000n],
+    // query: { enabled: approveTokenOne },
   });
   const { data: approveTokenTwoRequest } = useSimulateContract({
-    address: tokenOne,
+    address: tokenTwo,
+    abi,
     functionName: "approve",
-    args: [tokenTwo, 0n],
+    args: [Contracts.Router.address, 100000000n],
     query: { enabled: approveTokenTwo },
   });
-
   if (approveTokenOne) {
     return { data: approveTokenOneRequest };
   }
