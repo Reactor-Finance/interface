38803177b419e6fc5b916033456f74a43c1ae4fd
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2025-02-14 10:57:23 -0800

    feat: added primary color for checked slider

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -19,7 +19,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-4 w-4 rounded-full bg-[#E0E0E0] shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+        "pointer-events-none block h-4 w-4 rounded-full data-[state=checked]:bg-primary-200 bg-neutral-500 shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
       )}
     />
   </SwitchPrimitives.Root>
