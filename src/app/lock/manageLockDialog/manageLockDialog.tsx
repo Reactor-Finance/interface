33a6c6eca96c42df67b6b3fcbbd7a89ede76fd90
Tarp <travistarpley0@gmail.com>
2025-02-17 13:03:21 -0800

    feat: added manage lock dropdown to parent container

--- src/app/lock/manageLockDialog/manageLockDialog.tsx
+++ src/app/lock/manageLockDialog/manageLockDialog.tsx
@@ -10,80 +10,89 @@ import ExtendContent from "./extendContent";
 import TransferContent from "./transferContent";
 import MergeContent from "./mergeContent";
 import WithdrawContent from "./withdrawContent";
+import ManageLockDropdown from "./manageLockDropdown";
+import { ManageLockDialogProvider } from "./manageLockDialogProvider";
 
 export default function ManageLockDialog() {
   const [open, setOpen] = React.useState(false);
   return (
-    <div>
-      <Dialog open={open} onOpenChange={setOpen}>
-        <Button onClick={() => setOpen(!open)} variant={"primary"} size="md">
-          Claim All Lock Rewards
-        </Button>
-        <DialogContent position="static">
-          <DialogTitle className="text-lg">
-            Manage your <span className="text-primary-400">lock</span>
-          </DialogTitle>
-          <Tabs>
-            <TabsList display={"grow"} colors={"transparent"}>
-              <TabsTrigger
-                border="primary-1"
-                display="grow"
-                value="increase"
-                colors="white"
-              >
-                Increase
-              </TabsTrigger>
-              <TabsTrigger
-                border="primary-1"
-                display="grow"
-                value="extend"
-                colors="white"
-              >
-                Extend
-              </TabsTrigger>
-              <TabsTrigger
-                border="primary-1"
-                display="grow"
-                value="transfer"
-                colors="white"
-              >
-                Transfer
-              </TabsTrigger>
-              <TabsTrigger
-                border="primary-1"
-                display="grow"
-                value="withdraw"
-                colors="white"
-              >
-                Withdraw
-              </TabsTrigger>
-              <TabsTrigger
-                border="primary-1"
-                display="grow"
-                value="merge"
-                colors="white"
-              >
-                Merge
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="increase">
-              <IncreaseContent />
-            </TabsContent>
-            <TabsContent value="extend">
-              <ExtendContent />
-            </TabsContent>
-            <TabsContent value="transfer">
-              <TransferContent />
-            </TabsContent>
-            <TabsContent value="merge">
-              <MergeContent />
-            </TabsContent>
-            <TabsContent value="withdraw">
-              <WithdrawContent />
-            </TabsContent>
-          </Tabs>
-        </DialogContent>
-      </Dialog>
-    </div>
+    <ManageLockDialogProvider>
+      <div>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <Button onClick={() => setOpen(!open)} variant={"primary"} size="md">
+            Claim All Lock Rewards
+          </Button>
+
+          <DialogContent position="static">
+            <DialogTitle className="text-lg">
+              Manage your <span className="text-primary-400">lock</span>
+            </DialogTitle>
+
+            <Tabs>
+              <TabsList display={"grow"} colors={"transparent"}>
+                <TabsTrigger
+                  border="primary-1"
+                  display="grow"
+                  value="increase"
+                  colors="white"
+                >
+                  Increase
+                </TabsTrigger>
+                <TabsTrigger
+                  border="primary-1"
+                  display="grow"
+                  value="extend"
+                  colors="white"
+                >
+                  Extend
+                </TabsTrigger>
+                <TabsTrigger
+                  border="primary-1"
+                  display="grow"
+                  value="transfer"
+                  colors="white"
+                >
+                  Transfer
+                </TabsTrigger>
+                <TabsTrigger
+                  border="primary-1"
+                  display="grow"
+                  value="withdraw"
+                  colors="white"
+                >
+                  Withdraw
+                </TabsTrigger>
+                <TabsTrigger
+                  border="primary-1"
+                  display="grow"
+                  value="merge"
+                  colors="white"
+                >
+                  Merge
+                </TabsTrigger>
+              </TabsList>
+              <div className="py-4">
+                <ManageLockDropdown />
+              </div>
+              <TabsContent value="increase">
+                <IncreaseContent />
+              </TabsContent>
+              <TabsContent value="extend">
+                <ExtendContent />
+              </TabsContent>
+              <TabsContent value="transfer">
+                <TransferContent />
+              </TabsContent>
+              <TabsContent value="merge">
+                <MergeContent />
+              </TabsContent>
+              <TabsContent value="withdraw">
+                <WithdrawContent />
+              </TabsContent>
+            </Tabs>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </ManageLockDialogProvider>
   );
 }
