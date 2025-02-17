811f3290155182e45019552ef9030fbe8f0bafe7
Tarp <travistarpley0@gmail.com>
2025-02-17 11:33:02 -0800

    feat: rctInput added input props

--- src/app/lock/rctInput.tsx
+++ src/app/lock/rctInput.tsx
@@ -1,9 +1,9 @@
-import Input from "@/components/ui/input";
+import Input, { InputProps } from "@/components/ui/input";
 import Image from "next/image";
 import React from "react";
 import symbl from "@/assets/reactor-symbol.svg";
 
-export default function RctInput() {
+export default function RctInput(props: InputProps) {
   return (
     <div className=" focus-within:ring-1  ring-neutral-200 rounded-md bg-neutral-1000 border-neutral-950 flex border-[1px]  px-4">
       <div className="border-r-[1px] gap-x-1 flex border-neutral-900 py-2 pr-4">
@@ -11,6 +11,7 @@ export default function RctInput() {
         <h2>RCT</h2>
       </div>
       <Input
+        {...props}
         ring="none"
         className="border-none peer bg-transparent flex-grow"
       />
