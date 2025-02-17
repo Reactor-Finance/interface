5db6f622301ba6cce81c1598b1ca430413af8f79
Tarp <travistarpley0@gmail.com>
2025-02-17 12:25:01 -0800

    feat: added RCT_DECIMALS

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,3 +2,4 @@ export const ASSET_REPO =
   "https://raw.githubusercontent.com/SIR-trading/assets/master";
 export const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 export const TWO_YEARS = 62208000;
+export const RCT_DECIMALS = 18;
