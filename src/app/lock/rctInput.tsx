aaa1aeaa65bf04050f4f820db6ac911ed852ae53
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2025-02-16 21:25:48 -0800

    feat: focus ring

--- src/app/lock/rctInput.tsx
+++ src/app/lock/rctInput.tsx
@@ -5,7 +5,7 @@ import symbl from "@/assets/reactor-symbol.svg";
 
 export default function RctInput() {
   return (
-    <div className=" focus-within:ring ring-1 ring-neutral-200 rounded-md bg-neutral-1000 border-neutral-900 flex border-[1px]  px-4">
+    <div className=" focus-within:ring-1  ring-neutral-200 rounded-md bg-neutral-1000 border-neutral-950 flex border-[1px]  px-4">
       <div className="border-r-[1px] gap-x-1 flex border-neutral-900 py-2 pr-4">
         <Image src={symbl} width={24} height={24} alt="Reactor Ticker" />
         <h2>RCT</h2>
