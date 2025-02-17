928ac053e23a3628b6637fde9c224854b8e3692c
Tarp <travistarpley0@gmail.com>
2025-02-17 11:43:20 -0800

    feat: added TWO years in seconds constant

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,3 +1,4 @@
 export const ASSET_REPO =
   "https://raw.githubusercontent.com/SIR-trading/assets/master";
 export const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+export const TWO_YEARS = 62208000;
