b86a98ac708079b1a32c94b72289f823885f7840
Tarp <travistarpley0@gmail.com>
2025-02-17 21:01:36 -0800

    feat: adding approval and reset

--- src/app/lock/manageLockDialog/withdrawContent.tsx
+++ src/app/lock/manageLockDialog/withdrawContent.tsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Alert } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import { useSimulateContract, useWriteContract } from "wagmi";
+import {
+  useSimulateContract,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+} from "wagmi";
 import { Contracts } from "@/lib/contracts";
-import { parseUnits } from "viem";
+import useApproveVeRct from "./hooks/useApproveVeRct";
+import useGetLockApproval from "./hooks/useGetLockApproval";
+import { useQueryClient } from "@tanstack/react-query";
+import { useLockProvider } from "../lockProvider";
 
 export default function WithdrawContent() {
-  const tokenId = "1234";
-  const { data: withdrawSimulation } = useSimulateContract({
-    ...Contracts.VotingEscrow,
-    functionName: "withdraw",
-    args: [parseUnits(tokenId, 0)],
-  });
-  const { writeContract } = useWriteContract();
+  const { selectedLockToken } = useLockProvider();
+  const { data: withdrawSimulation, queryKey: withdrawKey } =
+    useSimulateContract({
+      ...Contracts.VotingEscrow,
+      functionName: "withdraw",
+      args: [selectedLockToken?.id ?? 0n],
+    });
+  const { data: approved, queryKey: getLockKey } = useGetLockApproval();
+  const { data: approveSimulation } = useApproveVeRct();
+  const { writeContract, reset, data: hash } = useWriteContract();
+  const { isSuccess, isError } = useWaitForTransactionReceipt({ hash });
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    if (isSuccess) {
+      if (!approved) {
+        queryClient.invalidateQueries({ queryKey: getLockKey });
+        queryClient.invalidateQueries({ queryKey: withdrawKey });
+      }
+      reset();
+    }
+  }, [
+    isSuccess,
+    isError,
+    reset,
+    approved,
+    queryClient,
+    getLockKey,
+    withdrawKey,
+  ]);
   const onSubmit = () => {
+    if (!approved && approveSimulation) {
+      writeContract(approveSimulation.request);
+      return;
+    }
     if (withdrawSimulation) {
       writeContract(withdrawSimulation.request);
     }
@@ -21,8 +54,13 @@ export default function WithdrawContent() {
   return (
     <div className="space-y-4 pt-4">
       <Alert colors="muted">Withdraw veRCT from your expired locks.</Alert>
-      <Button onClick={onSubmit} disabled size="submit" variant="primary">
-        Approve
+      <Button
+        onClick={onSubmit}
+        disabled={Boolean(!withdrawSimulation)}
+        size="submit"
+        variant="primary"
+      >
+        Withdraw
       </Button>
     </div>
   );
