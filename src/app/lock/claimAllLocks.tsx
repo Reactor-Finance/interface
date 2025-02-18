545e0e39e749ee51d5d25ed43bf464a6bef1bf1c
Tarp <travistarpley0@gmail.com>
2025-02-17 18:52:08 -0800

    feat: added Claim All Locks write

---
+++ src/app/lock/claimAllLocks.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Contracts } from "@/lib/contracts";
+import React, { useMemo } from "react";
+import { useSimulateContract, useWriteContract } from "wagmi";
+import { useLockProvider } from "./lockProvider";
+
+export default function ClaimAllLocks() {
+  const { lockTokens } = useLockProvider();
+  const tokenIds = useMemo(
+    () => lockTokens.map((token) => token.id),
+    [lockTokens]
+  );
+  const { data: claimAllSimulation } = useSimulateContract({
+    ...Contracts.RewardsDistributor,
+    functionName: "claim_many",
+    args: [tokenIds],
+  });
+  const { writeContract } = useWriteContract();
+  const onSubmit = () => {
+    if (claimAllSimulation) {
+      writeContract(claimAllSimulation.request);
+    }
+  };
+  return (
+    <Button
+      onClick={onSubmit}
+      disabled={!Boolean(claimAllSimulation)}
+      variant={"primary"}
+      size="md"
+    >
+      Claim All Locks
+    </Button>
+  );
+}
