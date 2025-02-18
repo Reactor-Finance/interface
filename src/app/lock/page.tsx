6a509e6afba7b0a8201cba5931a9fae57bb89e50
Tarp <travistarpley0@gmail.com>
2025-02-17 18:26:19 -0800

    feat: moved lock table and added provider

--- src/app/lock/page.tsx
+++ src/app/lock/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import React from "react";
 import Headers from "@/components/ui/headers";
-import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 import PageMarginContainer from "@/components/ui/pageMarginContainer";
-import ManageLockDialog from "./manageLockDialog/manageLockDialog";
 import CreateLockDialog from "./createLockDialog/createLockDialog";
 import HiveTable from "./hiveTable/hiveTable";
+import LockTable from "./lockTable/lockTable";
+import { LockTableProvider } from "./lockTable/lockTableProvider";
+import { Button } from "@/components/ui/button";
 export default function Lock() {
   return (
     <div>
@@ -35,54 +35,17 @@ export default function Lock() {
           <Headers.InfoHeaderTwo popupContent={<div></div>}>
             Locks
           </Headers.InfoHeaderTwo>
-          <ManageLockDialog />
+
+          <Button variant={"primary"} size="md">
+            Claim All Locks
+          </Button>
         </div>
         <div className="pt-8"></div>
-        <table className="w-full pt-6">
-          <caption className="h-0 opacity-0">Locks Table</caption>
-          <thead className="text-neutral-400 text-sm">
-            <tr className="grid grid-cols-8 px-6 py-2 font-medium">
-              <th className="col-span-2 text-left">Lock ID</th>
-              <th>Voting Power</th>
-              <th>APR</th>
-              <th>Rewards</th>
-              <th>Unlock Date</th>
-              <th>Status</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="grid text-center rounded-sm grid-cols-8 items-center bg-neutral-1000 py-2 px-6">
-              <td className="bg-neutral-1000 text-left col-span-2">
-                Row 1, Cell 1
-              </td>
-              <td className="">
-                <span className="block">396 veRCT</span>
-                <span className="block text-neutral-500 text-[12px]">
-                  Locked 396 RCT
-                </span>
-              </td>
-              <td className="">11.22</td>
-              <td className="">0.00 RCT</td>
-              <td className="">5 June 2025</td>
-              <td className="">
-                <Badge border="one" colors="success">
-                  Success
-                </Badge>
-              </td>
-              <td>
-                <div className="flex gap-x-4 justify-end">
-                  <Button variant={"primary"} size="xs">
-                    Claim
-                  </Button>
-                  <Button variant={"outline"} size="xs">
-                    Test
-                  </Button>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <div>
+          <LockTableProvider>
+            <LockTable />
+          </LockTableProvider>
+        </div>
         <div className="pt-16"></div>
         <Headers.InfoHeaderTwo popupContent={<div></div>}>
           Hive
