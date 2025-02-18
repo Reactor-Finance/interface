51261a5bfc9bb0f7f11fdc7899a208bd5c433627
Tarp <travistarpley0@gmail.com>
2025-02-17 18:25:01 -0800

    feat: adjusted select bg

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -89,7 +89,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative bg-neutral-950 z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border " +
+        "relative bg-neutral-1000 z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border " +
           " text-white shadow-md data-[state=open]:animate-in" +
           " data-[state=closed]:animate-out data-[state=closed]:fade-out-0 " +
           "data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 " +
