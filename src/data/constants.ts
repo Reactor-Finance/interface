149812a15d82ea41aaf7414c96429cd5fd0b2b87
Tarp <travistarpley0@gmail.com>
2025-02-17 21:02:47 -0800

    feat: added constants

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,4 +2,5 @@ export const ASSET_REPO =
   "https://raw.githubusercontent.com/SIR-trading/assets/master";
 export const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 export const TWO_YEARS = 62208000;
+export const DAYS_14 = 60 * 60 * 24 * 14;
 export const RCT_DECIMALS = 18;
