7de9479f4d4b25f10a931c94b571cf0ad2cb9580
Tarp <travistarpley0@gmail.com>
2025-02-18 16:57:05 -0800

    feat: new QUOTE_REFETCH_INTERVAL const

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -3,4 +3,5 @@ export const ASSET_REPO =
 export const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 export const TWO_YEARS = 62208000;
 export const DAYS_14 = 60 * 60 * 24 * 14;
+export const QUOTE_REFETCH_INTERVAL = 1000 * 30;
 export const RCT_DECIMALS = 18;
