21eb648ee891e4c140b816381219cdd3a8aa79c7
Tarp <travistarpley0@gmail.com>
2025-02-18 16:59:32 -0800

    feat: added swap provider
    Has all swap state for orbs, reactor swap, etc

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -1,45 +1,41 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { ReactNode } from "react";
-import { Button } from "@/components/ui/button";
 import CurrencyInputs from "./currencyInputs";
+import { Card } from "@/components/ui/card";
+import SubmitButton from "@/components/shared/submitBtn";
+import { SwapProvider } from "./swapProvider";
 export default function Home() {
   return (
-    <div className="min-h-screen px-4  font-geistMono pt-4 ">
-      <Card className="min-w-[380px] p-4 border rounded-md">
-        <Tabs defaultValue="swap">
-          <TabsList size="md" colors="muted" display={"grow"}>
-            <TabsTrigger display={"grow"} value="swap">
-              Swap
-            </TabsTrigger>
-            <TabsTrigger display={"grow"} value="twap">
-              TWAP
-            </TabsTrigger>
-            <TabsTrigger display={"grow"} value="limit">
-              Limit
-            </TabsTrigger>
-          </TabsList>
-        </Tabs>
-        <div className="pt-4"></div>
-
-        <CurrencyInputs />
-        <div className="pt-4"></div>
-        <Button className="w-full" size="md" variant="primary">
-          Connect Wallet
-        </Button>
-      </Card>
-    </div>
-  );
-}
-function Card({
-  className,
-  children,
-}: {
-  className: string;
-  children: ReactNode;
-}) {
-  return (
-    <div className={"border-neutral-700  border rounded-md " + className}>
-      {children}
-    </div>
+    <SwapProvider>
+      <div className="min-h-[85vh] px-4 font-geistMono pt-12">
+        <div className="mx-auto w-[440px]">
+          <div className="py-4">
+            <div className="py-2 flex justify-between">
+              <h1 className="text-primary-400 text-[44px]">Trade</h1>
+              <div></div>
+            </div>
+            <Tabs defaultValue="swap">
+              <TabsList size="md" colors="muted" display={"grow"}>
+                <TabsTrigger display={"grow"} value="swap">
+                  Swap
+                </TabsTrigger>
+                <TabsTrigger display={"grow"} value="twap">
+                  TWAP
+                </TabsTrigger>
+                <TabsTrigger display={"grow"} value="limit">
+                  Limit
+                </TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
+          <Card className="min-w-[380px] p-0 rounded-md">
+            <CurrencyInputs />
+            <div className="pt-4"></div>
+          </Card>
+          <div className="">
+            <SubmitButton>Swap</SubmitButton>
+          </div>
+        </div>
+      </div>
+    </SwapProvider>
   );
 }
