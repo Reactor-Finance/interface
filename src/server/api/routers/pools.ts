9628e45cdbcfd92551cc63aa54120c2cc9091362
Tarp <travistarpley0@gmail.com>
2025-02-20 13:06:13 -0800

    refactor: move back to imported schema

--- src/server/api/routers/pools.ts
+++ src/server/api/routers/pools.ts
@@ -3,25 +3,13 @@ import {
   executeFindPool,
   findPoolSchema,
 } from "@/server/queries/pools/getFindPools";
-import { executeGetPools } from "@/server/queries/pools/getPools";
-import { z } from "zod";
+import { executeGetPools, FilterSchema } from "@/server/queries/pools/getPools";
 
 export const poolsRouter = createTRPCRouter({
-  getPools: publicProcedure
-    .input(
-      z.object({
-        isStable: z.boolean().optional(),
-        totalSupply_lt: z.number().optional(),
-        totalSupply_gt: z.number().optional(),
-        skip: z.number().optional(),
-        searchQuery: z.string().optional(),
-      })
-    )
-    .query(async ({ input }) => {
-      console.log(input, "INPUT");
-      const pools = await executeGetPools(input);
-      return pools;
-    }),
+  getPools: publicProcedure.input(FilterSchema).query(async ({ input }) => {
+    const pools = await executeGetPools(input);
+    return pools;
+  }),
   findPool: publicProcedure.input(findPoolSchema).query(async ({ input }) => {
     const pools = await executeFindPool({
       tokenOneAddress: input.tokenOneAddress.toLowerCase(),
