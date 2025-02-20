972975307ac43ebe22b159f5e913e6688288b1a1
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2025-02-19 22:10:24 -0800

    feat: added exclude and add to joinWheres util fn

--- src/server/queries/utils.ts
+++ src/server/queries/utils.ts
@@ -1,14 +1,23 @@
 /**
  * Joins a filter object into a where clause
  * ex. where:{$a:a, $b:b}
+ * @filter: obj
+ * @exclude: string[] - any filters to exclude from join
  */
-export function joinWheres(filter: Record<string, string | number | boolean>) {
-  const whereConditions = [];
+export function joinWheres(
+  filter: Record<string, string | number | boolean>,
+  { exclude, add }: { exclude?: string[]; add?: string[] }
+) {
+  let whereConditions = [];
   for (const key in filter) {
+    if (exclude?.includes(key)) {
+      continue;
+    }
     if (filter[key] !== undefined) {
       whereConditions.push(`${key}: $${key}`);
     }
   }
+  whereConditions = [...whereConditions, add];
   const whereClause = whereConditions.length
     ? `where: { ${whereConditions.join(", ")} }`
     : "";
