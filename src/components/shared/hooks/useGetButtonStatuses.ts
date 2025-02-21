1307c1749d89e0ff5ea31114fbd4e9a3afebd639
Tarp <travistarpley0@gmail.com>
2025-02-20 19:55:14 -0800

    feat(get-button-statuses): added isFetching check
    This is for when approval succeeded but fetching new allowance number

--- src/components/shared/hooks/useGetButtonStatuses.ts
+++ src/components/shared/hooks/useGetButtonStatuses.ts
@@ -4,16 +4,21 @@ interface Props {
   isLoading: boolean;
   isPending: boolean;
   needsApproval?: boolean | undefined;
+  isFetching: boolean | undefined;
 }
 export default function useGetButtonStatuses({
   isLoading,
   isPending,
   needsApproval,
+  isFetching,
 }: Props) {
   if (isLoading) {
     return {
       state: ButtonState.Loading,
     };
+  }
+  if (isFetching) {
+    return { state: ButtonState.Loading };
   }
   if (isPending) {
     return {
