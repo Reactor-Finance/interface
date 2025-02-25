de663b09b92dc2016ccdef8b07d4a54f99f101a9
Tarp <travistarpley0@gmail.com>
2025-02-24 18:40:16 -0800

    feat: starting vote styles

---
+++ src/app/voting/page.tsx
@@ -0,0 +1,135 @@
+"use client";
+import PoolHeader from "@/components/shared/poolHeader";
+import SearchInput from "@/components/shared/searchInput";
+import VrctDropdown from "@/components/shared/vrctDropdown";
+import Headers from "@/components/ui/headers";
+import PageMarginContainer from "@/components/ui/pageMarginContainer";
+import { SelectItem } from "@/components/ui/select";
+import { TableBody, TableRow } from "@/components/ui/table";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { TPoolType } from "@/lib/types";
+import React from "react";
+
+export default function Page() {
+  return (
+    <PageMarginContainer>
+      <div className="rounded-lg flex flex-col sm:flex-row justify-between items-stretch  gap-y-6 sm:gap-y-0 sm:gap-x-16">
+        <div className="text-left h-[120px] sm:w-1/2">
+          <Headers.GradiantHeaderOne colorOne="#A0055D" colorTwo="#836EF9">
+            Vote
+          </Headers.GradiantHeaderOne>
+          <p className=" pt-8 text-sm text-neutral-500">
+            Use your veRCT to vote for directing emissions to your desired
+            pools. Deposit voting incentives to encourage others to do the same.
+          </p>
+        </div>
+
+        <div className=" space-y-2 flex flex-col justify-between ">
+          <div className="flex justify-between gap-x-6">
+            <p className="text-neutral-400 text-sm">Total Fees</p>
+            <p className="text-neutral-100 text-sm">$165,000,000</p>
+          </div>
+          <div className="flex justify-between gap-x-6">
+            <p className="text-neutral-400 text-sm">Total Incentives:</p>
+            <p className="text-neutral-100 text-sm">$44,866,347</p>
+          </div>
+          <div className="flex justify-between gap-x-6">
+            <p className="text-neutral-400 text-sm">Total Rewards:</p>
+            <p className="text-neutral-100 text-sm">$44,866,347</p>
+          </div>
+          <div className="flex justify-between gap-x-6">
+            <p className="text-neutral-400 text-sm">Epoch #85 Ends in:</p>
+            <p className="text-neutral-100 text-sm">06:17:52:36</p>
+          </div>
+        </div>
+      </div>
+      <div className="pt-12"></div>
+      <h2 className="text-2xl">Pools</h2>
+
+      <div className="flex justify-between pt-4 items-center">
+        <Tabs defaultValue="all">
+          <TabsList>
+            <TabsTrigger value={"all"}>All</TabsTrigger>
+            <TabsTrigger value={"stable"}>Stable</TabsTrigger>
+            <TabsTrigger value={"vol"}>Volatile</TabsTrigger>
+            {/* <TabsTrigger value={TabValues.CONCENTRATED}> */}
+            {/*   Concentrated */}
+            {/* </TabsTrigger> */}
+          </TabsList>
+        </Tabs>
+        <div className="gap-x-4 flex ">
+          <div className="w-[285px]">
+            <SearchInput
+              className="bg-neutral-950 w-[285px]"
+              value={""}
+              setValue={(value) => {
+                console.log(value);
+              }}
+            ></SearchInput>
+          </div>
+          <div className="w-[300px]">
+            <VrctDropdown placeholder="Select your veRCT">
+              <SelectItem value="hello">Hello</SelectItem>
+            </VrctDropdown>
+          </div>
+        </div>
+      </div>
+      <div className="pt-4"></div>
+      <table className="w-full">
+        <thead>
+          <tr className="grid grid-cols-10 px-4 text-neutral-500 text-sm font-normal">
+            <th className="col-span-3 text-left">Pool Name</th>
+            <th>Pool Name</th>
+            <th>Pool Name</th>
+            <th>Pool Name</th>
+            <th>Pool Name</th>
+            <th>Pool Name</th>
+            <th>Pool Name</th>
+            <th>Pool Name</th>
+          </tr>
+        </thead>
+        <TableBody>
+          <ExampleRow />
+          <ExampleRow />
+          <ExampleRow />
+          <ExampleRow />
+          <ExampleRow />
+        </TableBody>
+      </table>
+    </PageMarginContainer>
+  );
+}
+function ExampleRow() {
+  return (
+    <TableRow cols="10">
+      <td className="col-span-3">
+        <PoolHeader
+          poolType={TPoolType.CONCENTRATED}
+          token0={{
+            symbol: "ETH",
+            address: "0x123",
+            decimals: 18,
+            logoURI: "https://example.com",
+            name: "Ethereum",
+            chainId: 1,
+          }}
+          token1={{
+            symbol: "ETH",
+            address: "0x123",
+            decimals: 18,
+            logoURI: "https://example.com",
+            name: "Ethereum",
+            chainId: 1,
+          }}
+        />
+      </td>
+      <td>!108,198,100</td>
+      <td>11.22%</td>
+      <td>~43,279.55</td>
+      <td>131331</td>
+      <td>131331</td>
+      <td>131331</td>
+      <td>131331</td>
+    </TableRow>
+  );
+}
