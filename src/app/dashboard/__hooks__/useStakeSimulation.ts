ceddfd3ce6b3ead8ee687cf51119fd091174a0d0
Tarp <travistarpley0@gmail.com>
2025-02-28 12:03:13 -0800

    feat: added disabled param

--- src/app/dashboard/__hooks__/useStakeSimulation.ts
+++ src/app/dashboard/__hooks__/useStakeSimulation.ts
@@ -5,14 +5,17 @@ import { useSimulateContract } from "wagmi";
 export default function useStakeSimulation({
   address,
   amount,
+  disabled,
 }: {
   amount: bigint;
   address: Address | undefined;
+  disabled: boolean;
 }) {
   return useSimulateContract({
     abi: GauageAbi,
     address: address ?? "0x",
     functionName: "deposit",
     args: [amount],
+    query: { enabled: !disabled },
   });
 }
