1a4d24b134feec88e8d22546565754f01be5a969
Tarp <travistarpley0@gmail.com>
2025-03-02 14:58:49 -0800

    feat: improt

--- src/components/transactionSpinner.tsx
+++ src/components/transactionSpinner.tsx
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
 import Spinner from "./ui/spinner";
-import { useTransactionToastProvider } from "@/providers/TransactionToastProvider";
+import { useTransactionToastProvider } from "@/contexts/transactionToastProvider";
 
 export default function TransactionSpinner() {
   const { txReceipt } = useTransactionToastProvider();
   if (!txReceipt.isLoading) return;
   return (
     <div className="absolute z-50 right-[34px] top-[74px]">
-      <Spinner track="neutral500" />
+      <Spinner />
     </div>
   );
 }
