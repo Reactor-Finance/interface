547b057d3eb2a932484c526d0c04dd1b96023d98
Tarp <travistarpley0@gmail.com>
2025-03-03 13:24:36 -0800

    feat(initialize-liq): added proper symbols, fixed input bug
    fixed bug coming from quoteLiquidity which doens't exist on init liq

--- src/app/liquidity/add-liquidity/__components__/liquidityCard/addLiquidityInfo.tsx
+++ src/app/liquidity/add-liquidity/__components__/liquidityCard/addLiquidityInfo.tsx
@@ -3,8 +3,15 @@ import { useMemo } from "react";
 interface Props {
   amount0: string;
   amount1: string;
+  token0Symbol: string | undefined;
+  token1Symbol: string | undefined;
 }
-export default function AddLiquidityInfo({ amount0, amount1 }: Props) {
+export default function AddLiquidityInfo({
+  amount0,
+  amount1,
+  token0Symbol,
+  token1Symbol,
+}: Props) {
   const { result0, result1 } = useMemo(() => {
     if (isNaN(Number(amount0)) || isNaN(Number(amount1))) {
       return { result0: 0, result1: 0 };
@@ -25,11 +32,15 @@ export default function AddLiquidityInfo({ amount0, amount1 }: Props) {
       <div className="pt-1"></div>
       <div className="space-y-1">
         <div className="flex text-neutral-300 text-sm justify-between">
-          <span>USDC per USDT</span>
+          <span>
+            {token0Symbol} per {token1Symbol}
+          </span>
           <span>{result0}</span>
         </div>
         <div className="flex text-neutral-300 text-sm justify-between">
-          <span>USDC per USDT</span>
+          <span>
+            {token1Symbol} per {token0Symbol}
+          </span>
           <span>{result1}</span>
         </div>
       </div>
