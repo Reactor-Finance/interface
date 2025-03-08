8140d871e0c3cbaeb8cf352cae36a0e8fa758094
Tarp <travistarpley0@gmail.com>
2025-03-08 15:01:32 -0800

    feat: added submitbtn

--- src/app/swap/__components__/newSwapView.tsx
+++ src/app/swap/__components__/newSwapView.tsx
@@ -3,12 +3,18 @@ import { ArrowDown, ChevronDown } from "lucide-react";
 import React from "react";
 import wallet from "@/assets/wallet.svg";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function NewSwapView() {
   return (
-    <div className="space-y-1 relative">
-      <SwapCard />
-      <SwapCard />
+    <div className="space-y-1">
+      <div className="space-y-1 relative">
+        <SwapCard />
+        <SwapCard />
+        <Button size="submit" variant="primary">
+          Swap
+        </Button>
+      </div>
       <button className="h-14 flex items-center justify-center rounded-full w-14 bg-black absolute left-1/2 top-1/2 -translate-y-[calc(50%+4px)] -translate-x-1/2">
         <div className="h-12 w-12 rounded-full bg-neutral-950 flex items-center justify-center">
           <ArrowDown className="text-neutral-300" size={18} />
@@ -20,7 +26,7 @@ export default function NewSwapView() {
 
 function SwapCard() {
   return (
-    <div className="rounded-[24px] bg-[#303136] border border-[#43444C] space-y-3 p-6 ">
+    <div className="rounded-[16px] bg-[#303136] border border-[#43444C] space-y-3 p-6 ">
       <h2 className="text-sm">Sell</h2>
       <div className="flex items-center gap-x-4 ">
         <Input
