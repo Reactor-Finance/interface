82cb5913dfa3cdae1e507d393667a112b1500871
Tarp <travistarpley0@gmail.com>
2025-03-07 16:26:24 -0800

    feat: return queryKey

--- src/lib/hooks/useGetPairs.ts
+++ src/lib/hooks/useGetPairs.ts
@@ -14,18 +14,11 @@ export function useGetPairs({
   const chainId = useChainId();
   const { address = zeroAddress } = useAccount();
   const pairHelper = useMemo(() => PAIR_HELPER[chainId], [chainId]);
-  const { data = [] } = useReadContract({
+  const { data = [], queryKey = [] } = useReadContract({
     ...PairHelper,
     address: pairHelper,
     functionName: "getAllPair",
     args: [address, limit, offset],
   });
-
-  // useWatchBlocks({
-  //   onBlock: () => {
-  //     void refetch();
-  //   },
-  // });
-
-  return data;
+  return data.map((pair) => ({ ...pair, queryKey }));
 }
