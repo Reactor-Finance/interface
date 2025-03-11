288392132cb9c92ed924f02b83a1eacd3792c15a
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2025-03-10 23:12:37 -0700

    feat: swap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,5 @@
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Card } from "@/components/ui/card";
-import PageMarginContainer from "@/components/ui/pageMarginContainer";
-import NewSwapView from "./swap/__components__/newSwapView";
+import SwapPage from "./swap/swapPage";
 
 export default function Swap() {
-  return (
-    <PageMarginContainer>
-      <div className="mx-auto w-[440px]">
-        <div className="pb-4">
-          <div className="py-2 flex justify-between">
-            <h1 className="text-primary-400 text-[44px]">Trade</h1>
-            <div></div>
-          </div>
-          <Tabs defaultValue="swap">
-            <TabsList size="md" colors="muted" display={"grow"}>
-              <TabsTrigger display={"grow"} value="swap">
-                Swap
-              </TabsTrigger>
-              <TabsTrigger display={"grow"} value="twap">
-                TWAP
-              </TabsTrigger>
-              <TabsTrigger display={"grow"} value="limit">
-                Limit
-              </TabsTrigger>
-            </TabsList>
-          </Tabs>
-        </div>
-        <Card className="min-w-[380px] p-0 rounded-md">
-          {/* <SwapView /> */}
-          <NewSwapView />
-        </Card>
-      </div>
-    </PageMarginContainer>
-  );
+  return <SwapPage />;
 }
