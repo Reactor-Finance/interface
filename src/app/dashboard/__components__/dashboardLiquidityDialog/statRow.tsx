5431e98808094d584fd3166d2d3cd0f2c58a3184
Tarp <travistarpley0@gmail.com>
2025-03-14 11:04:41 -0700

    fix: hello text

--- src/app/dashboard/__components__/dashboardLiquidityDialog/statRow.tsx
+++ src/app/dashboard/__components__/dashboardLiquidityDialog/statRow.tsx
@@ -16,8 +16,8 @@ export function StatRow({
     <div className="flex justify-between  text-sm">
       <span className=" text-neutral-300">{title}</span>
       {isLoading ? (
-        <div className="bg-neutral-700/70 rounded-md animate-pulse">
-          <span className="text-transparent">hello</span>
+        <div className="bg-neutral-800/70 rounded-md animate-pulse">
+          <span className="text-transparent w-5 select-none">hello</span>
         </div>
       ) : (
         <span className="text-white">
