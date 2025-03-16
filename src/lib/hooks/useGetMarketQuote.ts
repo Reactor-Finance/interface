fd1acf4ff95399cac4fa816d5190daf3110d8581
Tarp <travistarpley0@gmail.com>
2025-03-16 13:10:33 -0700

    feat: query optimizations on useGetMarketQuote

--- src/lib/hooks/useGetMarketQuote.ts
+++ src/lib/hooks/useGetMarketQuote.ts
@@ -2,9 +2,11 @@ import { useChainId, useReadContract } from "wagmi";
 import * as Oracle from "../abis/Oracle";
 import { useMemo } from "react";
 import { ORACLE } from "@/data/constants";
+import { zeroAddress } from "viem";
 
 // Quote is in USD
 export function useGetMarketQuote({
+  disabled,
   tokenAddress,
   value,
   staleTime = 0,
@@ -12,6 +14,7 @@ export function useGetMarketQuote({
   tokenAddress: `0x${string}`;
   value: bigint;
   staleTime?: number;
+  disabled?: boolean;
 }) {
   const chainId = useChainId();
   const oracle = useMemo(() => ORACLE[chainId], [chainId]);
@@ -26,6 +29,7 @@ export function useGetMarketQuote({
     args: [tokenAddress, value],
     query: {
       staleTime,
+      enabled: tokenAddress !== zeroAddress && !disabled,
     },
   });
 
