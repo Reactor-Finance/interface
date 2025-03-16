f2c4d03bf9a733f4eb3a36e22a1991a17e5e3092
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2025-03-15 20:37:10 -0700

    fix: remove wmon

--- src/app/liquidity/add-liquidity/__components__/liquidityCard/assetSymbolAndName.tsx
+++ src/app/liquidity/add-liquidity/__components__/liquidityCard/assetSymbolAndName.tsx
@@ -1,6 +1,5 @@
 import ImageWithFallback from "@/components/shared/imageWithFallback";
 import { useTokenlistContext } from "@/contexts/tokenlistContext";
-import { checkMon } from "@/lib/checkMon";
 import { TAddress } from "@/lib/types";
 import { useMemo } from "react";
 
@@ -30,7 +29,7 @@ export default function AssetSymbolAndName({
         src={token.logoURI}
         alt={token.symbol}
       />
-      <span className="text-[13px]">{checkMon(token.symbol)}</span>
+      <span className="text-[13px]">{token.symbol}</span>
     </div>
   );
 }
