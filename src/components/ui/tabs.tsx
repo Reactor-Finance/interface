20954f5a77c7a66f61c74772eb4efee3472a92e2
Tarp <travistarpley0@gmail.com>
2025-03-17 11:02:19 -0700

    fix: mobile padding styles

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -54,7 +54,7 @@ const TabsList = React.forwardRef<
 TabsList.displayName = TabsPrimitive.List.displayName;
 
 const tabsTriggerVariants = cva(
-  `inline-flex items-center  justify-center whitespace-nowrap rounded-sm px-1 md:px-3
+  `inline-flex items-center  justify-center whitespace-nowrap rounded-sm px-2 md:px-3
     py-1.5 text-sm font-medium ring-offset-background transition-all
     focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring
     focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50
