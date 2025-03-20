10f6c8b79cddb39ccc71144586969df54819a949
Tarp <travistarpley0@gmail.com>
2025-03-19 18:25:23 -0700

    feat: adjusted voting power styles

--- src/app/voting/__components__/votePower.tsx
+++ src/app/voting/__components__/votePower.tsx
@@ -26,19 +26,21 @@ export default function VotePower() {
   return (
     <>
       <VoteDialog setOpen={setOpen} open={open} />
-      <div className="py-3 z-20 px-6 flex gap-x-10 fixed rounded-md bottom-8 border border-neutral-950 -translate-x-1/2 left-1/2 items-center bg-neutral-1000">
-        <div className="flex gap-x-4 items-center">
-          <span>
-            Voting power used:{" "}
-            <span className="text-blue-light">{totalPercent} %</span>
-          </span>
-          <div className="bg-purple-400/10 text-purple-400 h-7 w-7 flex justify-center items-center rounded-full">
+      <div className="py-3 z-20 px-6 flex justify-between gap-x-10 w-[460px] fixed rounded-md bottom-8 border border-neutral-950 -translate-x-1/2 left-1/2 items-center bg-neutral-1000">
+        <div className="flex gap-x-1 items-center">
+          <div>Voting power used: </div>
+          <div className="text-blue-light flex">
+            <div className="text-blue-light w-4">{totalPercent}</div>
+            <div>%</div>
+          </div>
+          <div className="bg-purple-400/10 ml-2 text-purple-400 h-7 w-7 flex justify-center items-center rounded-full">
             {amount}
           </div>
         </div>
         <Button
           onClick={() => setOpen(true)}
           variant="primary"
+          size="md"
           disabled={amount <= 0}
         >
           Vote
