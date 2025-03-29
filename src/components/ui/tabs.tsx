dcb432af283f2af89d37ee8fd3f7a074a82b751b
Tarp <travistarpley0@gmail.com>
2025-03-28 18:21:56 -0700

    fix: remove react-hero dep issue

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -4,8 +4,7 @@ import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 
 import { cn } from "@/lib/utils";
-import { VariantProps } from "@heroui/react";
-import { cva } from "class-variance-authority";
+import { cva, VariantProps } from "class-variance-authority";
 
 const Tabs = TabsPrimitive.Root;
 // Define the `cva` function for styling variants
